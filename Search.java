6329a0ff5182597809f19e89b68da4dab50f36e4
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-02 14:44:28 +0530

    Linear Search Added

--- Search.java
+++ Search.java
@@ -1,8 +1,20 @@
 Class Search
 {
+    public static void Linear Search(int arr[],int element)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==element)
+                {
+                    System.out.println("Element Found at index: "+i);
+                    return;
+                }
+        }
+        System.out.println("Element Not Found");  
+    }
     public static void main(String[] args)  
     {
-        /*Linear Search Code*/ 
+       
     }
 
 }

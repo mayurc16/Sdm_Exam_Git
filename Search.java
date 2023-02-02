d5e60341bcf09deeee697e07e262ab6ba1ad4d11
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-02 15:08:14 +0530

    Menu driven program

--- Search.java
+++ Search.java
@@ -1,22 +1,26 @@
 Class Search
 {
-    public static boolean BinarySearch(int arr[],int element,int si, int ei)
-    {
-        if(si>ei)
-            return false;
-        int mid =arr.length/2;
-        if(arr[mid]==element) 
-        {
-            return true;
-        }   
-        if(arr[mid]>=element) 
-            BinarySearch(arr, element, mid, ei);
-        else
-            BinarySearch(arr, element, si,mid);
-    }   
+   
     public static void main(String[] args)  
     {
+       /*Menu Driven Program*/ 
        
+       int i ;
+       do
+       {
+        System.out.println("01. Linear Search");
+        System.out.println("01. Binary Search");
+        System.out.println("0. Exit");
+        i=/*Input from user*/ 
+            Switch(i)
+            {
+                case 1:LinearSearch(); break;
+                case 2:BinarySearch(); break;
+                case 0:i=0; break;
+            }
+       }while (i==0);
+       
+
     }
 
 }

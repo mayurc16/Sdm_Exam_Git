dae54faab5cf840a684fcabb1c74ffed693635ab
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-02 14:55:49 +0530

    Binary search added removed linear search

--- Search.java
+++ Search.java
@@ -1,17 +1,5 @@
 Class Search
 {
-    public static void Linear Search(int arr[],int element)
-    {
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==element)
-                {
-                    System.out.println("Element Found at index: "+i);
-                    return;
-                }
-        }
-        System.out.println("Element Not Found");  
-    }
     public static boolean BinarySearch(int arr[],int element,int si, int ei)
     {
         if(si>ei)

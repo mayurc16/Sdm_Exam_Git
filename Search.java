9655bea0439f1c4a660a90cf0d0c39a53138b750
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-02 14:53:56 +0530

    Binary Search added

--- Search.java
+++ Search.java
@@ -12,6 +12,20 @@ Class Search
         }
         System.out.println("Element Not Found");  
     }
+    public static boolean BinarySearch(int arr[],int element,int si, int ei)
+    {
+        if(si>ei)
+            return false;
+        int mid =arr.length/2;
+        if(arr[mid]==element) 
+        {
+            return true;
+        }   
+        if(arr[mid]>=element) 
+            BinarySearch(arr, element, mid, ei);
+        else
+            BinarySearch(arr, element, si,mid);
+    }   
     public static void main(String[] args)  
     {
        
